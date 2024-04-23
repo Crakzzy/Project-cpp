69a058ffc339e556b8b88b422e5d998188afa79d
Martin Valko <matinko.valko@gmail.com>
2024-04-23 13:48:13 +0200

    3. time now

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <chrono>
+
+using namespace std::chrono;
+using namespace std;
 
 int main() {
+
+    milliseconds ms = duration_cast< milliseconds >(
+            system_clock::now().time_since_epoch()
+    );
+
+    cout << ms.count() << endl;
+
     return 0;
 }
