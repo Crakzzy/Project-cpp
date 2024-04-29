c65b8260b674c8dae9d0558f2d3c2984da9a510f
Matej Púčik <matej.pucik@student.ssnd.sk>
2024-04-29 20:34:34 +0200

    4. bubble sort

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <chrono>
+#include <vector>
 
 using namespace std::chrono;
 using namespace std;
+
+void bubbleSort(vector<int>& array) {
+    int n = array.size();
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (array[j] > array[j + 1]) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
 
 int main() {
 
