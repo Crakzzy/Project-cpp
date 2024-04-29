f5611c0c7e93de0bc5d3abdf4d02cdb5b27d47ad
Matej Púčik <matej.pucik@student.ssnd.sk>
2024-04-29 20:36:00 +0200

    4. selection sort

--- main.cpp
+++ main.cpp
@@ -4,6 +4,21 @@
 using namespace std::chrono;
 using namespace std;
 
+void selectionSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+}
+
 int main() {
 
     milliseconds ms = duration_cast< milliseconds >(
